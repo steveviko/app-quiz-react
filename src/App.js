b76f7a94af6668920898c834a002f182cc1eba89
steven opio <stevenviko@gmail.com>
2020-11-01 23:37:16 +0300

    Update App.js file and index.html file

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ const API_URL = "https://opentdb.com/api.php?amount=10&category=14&difficulty=ea
 
 function App() {
   return (
-    <div className="App">
-    <h2>Hello guys</h2>
+    <div className="container">
+      <div>questions place holder</div>
+      <div>
+        <div>answer placeholder 1</div>
+        <div>answer placeholder 2</div>
+        <div>answer placeholder 3</div>
+        <div>answer placeholder 4</div>
+      </div>
     </div>
   );
 }
