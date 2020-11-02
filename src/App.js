39a5d686b8f16d8eb1cd0808f30f9ad337634134
steven opio <stevenviko@gmail.com>
2020-11-02 06:56:29 +0300

    Add jsx on App component

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ const API_URL = "https://opentdb.com/api.php?amount=10&category=14&difficulty=ea
 function App() {
   return (
     <div className="container">
-      <div>questions place holder</div>
-      <div>
-        <div>answer placeholder 1</div>
-        <div>answer placeholder 2</div>
-        <div>answer placeholder 3</div>
-        <div>answer placeholder 4</div>
+      <div className="bg-white text-purple-800 p-10 rounded-lg shadow-md">
+        <h2 className="text-2xl"> 1. questions place holder </h2>
+        
+        </div>
+      <div className="flex flex-wrap mt-4 justify-around bg-white">
+        <button className="bg-white w-5/12 p-4 text-purple-800 font-semibold rounded shadow mb-4">answer placeholder 2</button >
+        <button className="bg-white w-5/12 p-4 text-purple-800 font-semibold rounded shadow mb-4">answer placeholder 3</button >
+        <button className="bg-white w-5/12 p-4 text-purple-800 font-semibold rounded shadow">answer placeholder 1</button >
+        <button className="bg-white w-5/12 p-4 text-purple-800 font-semibold rounded shadow" >answer placeholder 4</button >
       </div>
     </div>
   );
