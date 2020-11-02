75d5cf752f3072885c2011a62e2e72884ae82c0b
steven opio <stevenviko@gmail.com>
2020-11-02 07:40:02 +0300

    Update jsx App structure

--- src/App.js
+++ src/App.js
@@ -19,18 +19,27 @@ function App() {
 
   return (
     <div className="container">
+      {questions.length > 0 ? (
+  <div>
       <div className="bg-white text-purple-800 p-8 rounded-lg shadow-md">
-        <h2 className="text-2xl">{questions[0].question}</h2>
-        
-        </div>
-      <div className="grid grid-cols-2 gap-6 mt-6">
-        <button className="bg-white  p-4 text-purple-800 font-semibold rounded shadow">{questions[0].correct_answer}</button >
-        <button className="bg-white  p-4 text-purple-800 font-semibold rounded shadow">{questions[0].incorrect_answers[0]}</button >
-        <button className="bg-white  p-4 text-purple-800 font-semibold rounded shadow">{questions[0].incorrect_answers[1]}</button >
-        <button className="bg-white  p-4 text-purple-800 font-semibold rounded shadow" >{questions[0].incorrect_answers[2]}</button >
+      <h2 className="text-2xl">{questions[0].question}</h2>
       </div>
+
+      <div className="grid grid-cols-2 gap-6 mt-6">
+      <button className="bg-white  p-4 text-purple-800 font-semibold rounded shadow">{questions[0].correct_answer}</button >
+      <button className="bg-white  p-4 text-purple-800 font-semibold rounded shadow">{questions[0].incorrect_answers[0]}</button >
+      <button className="bg-white  p-4 text-purple-800 font-semibold rounded shadow">{questions[0].incorrect_answers[1]}</button >
+      <button className="bg-white  p-4 text-purple-800 font-semibold rounded shadow" >{questions[0].incorrect_answers[2]}</button >
+      </div>
+      
+  </div>
+      ) :(
+        <h2>The questions are Loadding .....please wait</h2>
+      )}
+      
     </div>
   );
+  
 }
 
 export default App;
